a6ff8111f5859e252dac84d6369df7dd4a909983
Andrew <AndrewSilver@mail.ru>
2023-12-11 19:57:12 +0300

    домашка готова

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Zadacha_1
+{
+    public static class Program
+    {
+        public static void Main(string[] arg)
+        {
+            string str = "ПрИвЕтИк";
+            string str1;
+            str1=str.ToLower();
+            System.Console.WriteLine($"Это слово в разных регистрах==>>  {str} ");
+            System.Console.WriteLine($"Это слово в нижнем регистрах==>>  {str1} ");
+
+        }
+    }
+}
---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Zadacha_1
+{
+    public static class Program
+    {
+        public static void Main(string[] arg)
+        {
+            string str = "рвал дед лавр";
+
+            System.Console.WriteLine($"Это слово в разных регистрах==>>  {str} ");
+
+        }
+        private static void Polindrom(string str)
+        {
+            int size = str.Length;
+            int count = 0;
+            while (str[count] == str[size - count] && count < size)
+
+
+            {
+
+                count++;
+
+            }
+            if (count == size / 2)
+            {
+                
+            }
+
+        }
+    }
+}
